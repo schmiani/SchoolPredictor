b20d2e85a8dd8e9b14e748a44e3c62250095cec7
schmiani <schmiani@gmail.com>
2014-08-09 23:52:01 -0400

    First template for webiste

--- dev/app/views.py
+++ dev/app/views.py
@@ -9,13 +9,23 @@ db= mdb.connect('localhost', 'root', 'password', 'school');
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template("index.html",
-        title = 'Home', user = { 'nickname': 'Miguel' },
-        )
+	return render_template("index.html")
+
+@app.route("/schools_json")
+def schools_json():
+    with db:
+        cur = db.cursor()
+        cur.execute("SELECT SCHOOL_NAME, 2012_2013, 2011_2012, 2010_11, 2009_10, 2008_09, 2007_08, 2006_07 FROM Multiyear")
+        query_results = cur.fetchall()
+    schools = []
+    for result in query_results:
+        schools.append(dict(school=result[0], grade2013=result[1], grade2012=result[2], grade2011=result[3], grade2010=result[4], \
+            grade2009=result[5], grade2008=result[6], grade2007=result[7])), 
+    return jsonify(dict(schools=schools))
 	
-@app.route("/jquery")
+@app.route("/dev")
 def index_jquery():
-    return render_template('index_js.html') 
+    return render_template('index_dev.html') 
 
 @app.route("/db_json")
 def cities_json():
