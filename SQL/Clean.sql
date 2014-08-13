00a692c5e91ace97f0571996ebfb2735d98ae919
schmiani <schmiani@gmail.com>
2014-08-13 14:48:32 -0400

    More cleaning

--- SQL/Clean.sql
+++ SQL/Clean.sql
@@ -26,3 +26,18 @@ SELECT School_Type FROM EMS_2008_All WHERE School_Type = 'MIDDLE SCHOOL';
 UPDATE EMS_2008_All SET School_Type = 'Middle' WHERE School_Type = 'MIDDLE SCHOOL';
 SELECT School_Type FROM EMS_2008_All WHERE School_Type = 'ELEMENTARY';
 UPDATE EMS_2008_All SET School_Type = 'Elementary' WHERE School_Type = 'ELEMENTARY';
+
+UPDATE EMS_2007_All SET Math_Median_Proficiency = NULL WHERE Math_Median_Proficiency = '';
+UPDATE EMS_2007_All SET ELA_Median_Proficiency = NULL WHERE ELA_Median_Proficiency = '';
+UPDATE EMS_2008_All SET Math_Median_Proficiency = NULL WHERE Math_Median_Proficiency = '';
+UPDATE EMS_2008_All SET ELA_Median_Proficiency = NULL WHERE ELA_Median_Proficiency = '';
+UPDATE EMS_2009_All SET Math_Median_Proficiency = NULL WHERE Math_Median_Proficiency = '';
+UPDATE EMS_2009_All SET ELA_Median_Proficiency = NULL WHERE ELA_Median_Proficiency = '';
+UPDATE EMS_2010_All SET Math_Median_Proficiency = NULL WHERE Math_Median_Proficiency = '';
+UPDATE EMS_2010_All SET ELA_Median_Proficiency = NULL WHERE ELA_Median_Proficiency = '';
+UPDATE EMS_2011_All SET Math_Average_Proficiency = NULL WHERE Math_Average_Proficiency = '';
+UPDATE EMS_2011_All SET ELA_Average_Proficiency = NULL WHERE ELA_Average_Proficiency = '';
+UPDATE EMS_2012_All SET Math_Average_Proficiency = NULL WHERE Math_Average_Proficiency = '';
+UPDATE EMS_2012_All SET ELA_Average_Proficiency = NULL WHERE ELA_Average_Proficiency = '';
+UPDATE EMS_2013_All SET Math_Average_Proficiency = NULL WHERE Math_Average_Proficiency = '';
+UPDATE EMS_2013_All SET ELA_Average_Proficiency = NULL WHERE ELA_Average_Proficiency = '';
