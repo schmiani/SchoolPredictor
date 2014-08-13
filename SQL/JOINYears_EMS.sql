53c703ede37214879f5c61d55ff52e14fcb6c0a3
schmiani <schmiani@gmail.com>
2014-08-13 14:49:41 -0400

    Add grades to view

--- SQL/JOINYears_EMS.sql
+++ SQL/JOINYears_EMS.sql
@@ -3,12 +3,19 @@ CREATE OR REPLACE VIEW AllYears_EMS AS
     SELECT 
         a.DBN, a.School, a.School_Type,
         b.Overall_Score AS 2012_13_SCORE,
+		b.Overall_Grade AS 2012_13_GRADE,
         c.Overall_Score AS 2011_12_SCORE,
+        c.Overall_Grade AS 2011_12_GRADE,
         d.Overall_Score AS 2010_11_SCORE,
+        d.Overall_Grade AS 2010_11_GRADE,
 		e.Overall_Score AS 2009_10_SCORE,
+		e.Grade AS 2009_10_GRADE,
 		f.Overall_Score AS 2008_09_SCORE,
+		f.Grade AS 2008_09_GRADE,
 		g.Overall_Score AS 2007_08_SCORE,
-		h.Overall_Score AS 2006_07_SCORE
+		g.Grade AS 2007_08_GARDE,
+		h.Overall_Score AS 2006_07_SCORE,
+		h.Grade AS 2006_07_GRADE
     FROM
         EMS_All_Info a
             LEFT JOIN
